09550fec16f09f7a9209baa822e0bffade486f10
Mohan <bagan.halder@yahoo.com>
2025-01-26 13:28:31 +0800

    message

--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
-// Hamburger menu
+// fetch All Products
+const productContainer = document.getElementById("products-container");
+fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((products) => {
+    console.log(products)
+    const productsItems = products.map((product) => {
+      const productsItemHTML = `
+        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a href="productdetails.html" class="block relative h-48 rounded overflow-hidden">
+            <img alt="ecommerce" class="object-cover object-center w-full h-full &"src="${product.image}">
+            </a>
+            <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">${product.category}</h3>
+                <h2 class="text-gray-900 title-font text-lg font-medium">${product.title}</h2>
+                <p class="mt-1">${product.price}</p>
+            </div>
+        </div>
+        `;
+      return productsItemHTML;
+    });
+    console.log(productsItems);
+    const productsHTML = productsItems.join("");
+    console.log(productsHTML);
+    productsContainer.innerHTML = productsHTML;
+  });
+
+
+const productsContainer = document.getElementById("products-container");
+fetch("https://fakestoreapi.com/products/categories");
+    // Hamburger menu start
 const hamburgerBtn = document.getElementById("hamburger-menu");
-const mobileNavbar = document.getElementById("mobile-navbar");
+// const mobileNavbar = document.getElementById("mobile-navbar");
 const closeBtn = document.getElementById("close-btn");
 console.log(mobileNavbar);
 console.log(hamburgerBtn);
-hamburgerBtn.addEventListener("click", function(){
-    console.log(mobileNavbar.classList)
-    mobileNavbar.classList.remove("hidden")
-    mobileNavbar.classList.add("flex")
-    hamburgerBtn.classList.add("hidden")
-    closeBtn.classList.remove("hidden")
-    console.log(mobileNavbar.classList)
-})
+hamburgerBtn.addEventListener("click", function () {
+  console.log(mobileNavbar.classList);
+  mobileNavbar.classList.remove("hidden");
+  mobileNavbar.classList.add("flex");
+  hamburgerBtn.classList.add("hidden");
+  closeBtn.classList.remove("hidden");
+  console.log(mobileNavbar.classList);
+});
 
-// hamburgerBtn.addEventListener("click", function () {
+// hamburgerBtn.addEventListener("click", function(){
 // //   console.log(mobileNavbar.classList);
 //     mobileNavbar.classList.toggle("hidden");
 //     mobileNavbar.classList.toggle("flex");
@@ -21,20 +51,14 @@ hamburgerBtn.addEventListener("click", function(){
 // });
 
 closeBtn.addEventListener("click", function () {
-    mobileNavbar.classList.add("hidden")
-    mobileNavbar.classList.remove("flex")
-    closeBtn.classList.add("hidden")
-    hamburgerBtn.classList.add("block")
-    hamburgerBtn.classList.remove("hidden")
-})
+mobileNavbar.classList.add("hidden");
+mobileNavbar.classList.remove("flex");
+closeBtn.classList.add("hidden");
+hamburgerBtn.classList.add("block");
+hamburgerBtn.classList.remove("hidden");
+});
 
-
-
-
-
-
-
-
+// Hamburger menu end
 
 // console.log("Hello World I am from Jajabor");
 /**
@@ -42,39 +66,39 @@ closeBtn.addEventListener("click", function () {
  * 2. Click on Shop Button
  * 3. url identify
  * 3. url--> shop.html
- * 
+ *
  */
 
 const shopBtn1 = document.querySelector(".shop-btn-1");
 // console.log(shopBtn1);
 shopBtn1.addEventListener("click", function () {
-    // console.log("This shop button is clicked");
-    console.log(window.location.href)
-    console.log("navigating to shop.html")
-    window.location.href = "http://127.0.0.1:5500/shop.html";
+  // console.log("This shop button is clicked");
+console.log(window.location.href);
+console.log("navigating to shop.html");
+window.location.href = "http://127.0.0.1:5500/shop.html";
 });
 
 const shopBtn2 = document.querySelector(".shop-btn-2");
 // console.log(shopBtn1);
 shopBtn2.addEventListener("click", function () {
   // console.log("This shop button is clicked");
-console.log(window.location.href);
-console.log("navigating to shop.html");
-window.location.href = "http://127.0.0.1:5500/shop.html";
+  console.log(window.location.href);
+  console.log("navigating to shop.html");
+  window.location.href = "http://127.0.0.1:5500/shop.html";
 });
 
 const shopBtn3 = document.querySelector(".shop-btn-3");
 // console.log(shopBtn1);
 shopBtn3.addEventListener("click", function () {
   // console.log("This shop button is clicked");
-console.log(window.location.href);
-console.log("navigating to shop.html");
-window.location.href = "http://127.0.0.1:5500/shop.html";
+  console.log(window.location.href);
+  console.log("navigating to shop.html");
+  window.location.href = "http://127.0.0.1:5500/shop.html";
 });
 
 /**
  * 1. select 3 slide
- * 2. close 2 slide 
+ * 2. close 2 slide
  * 3. 2 button identify
  * 4. add event to those 2 button
  * 5. next button--> First slide-> display :none+2nd slide--> display:block
@@ -87,7 +111,6 @@ const thirdSlide = document.getElementById("third-slide");
 const preBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 
-
 // console.log(firstSlide);
 // console.log(secondSlide);
 // console.log(thirdSlide);
@@ -97,52 +120,51 @@ const nextBtn = document.getElementById("next");
 // console.log(preBtn);
 // console.log(nextBtn);
 
-preBtn.addEventListener("click",function () {
-    // console.log("Previous Button is clicked")
-    const isFirstSlideActive = getComputedStyle(firstSlide).display == "flex";
-    const isSecondSlideActive = getComputedStyle(secondSlide).display == "flex";
-    const isThirdSlideActive = getComputedStyle(thirdSlide).display == "flex";
-    // console.log(isFirstSlideActive, /*isSecondSlideActive, isThirdSlideActive*/);
-    // console.log(isFirstSlideActive);
+preBtn.addEventListener("click", function () {
+  // console.log("Previous Button is clicked")
+  const isFirstSlideActive = getComputedStyle(firstSlide).display == "flex";
+  const isSecondSlideActive = getComputedStyle(secondSlide).display == "flex";
+  const isThirdSlideActive = getComputedStyle(thirdSlide).display == "flex";
+  // console.log(isFirstSlideActive, /*isSecondSlideActive, isThirdSlideActive*/);
+  // console.log(isFirstSlideActive);
 
-    if (isFirstSlideActive) {
-        firstSlide.style.display = "none";
-        secondSlide.style.display = "none";
-        thirdSlide.style.display = "flex";
-    } else if (isThirdSlideActive) {
-        thirdSlide.style.display = "none";
-        secondSlide.style.display = "flex";
-        firstSlide.style.display = "none";
-    } else if (isSecondSlideActive) {
-        secondSlide.style.display = "none";
-        firstSlide.style.display = "flex";
-        thirdSlide.style.display = "none";
-    }
+  if (isFirstSlideActive) {
+    firstSlide.style.display = "none";
+    secondSlide.style.display = "none";
+    thirdSlide.style.display = "flex";
+  } else if (isThirdSlideActive) {
+    thirdSlide.style.display = "none";
+    secondSlide.style.display = "flex";
+    firstSlide.style.display = "none";
+  } else if (isSecondSlideActive) {
+    secondSlide.style.display = "none";
+    firstSlide.style.display = "flex";
+    thirdSlide.style.display = "none";
+  }
 });
 
 nextBtn.addEventListener("click", function () {
-    // console.log("Next Button is clicked");
-    // console.log("firstSlide is getting out")
-    // firstSlide.style.display = "none"
-    // console.log("2nd slide is getting in")
-    // secondSlide.style.display = "flex"
-// const isFirstSlideActive = getComputedStyle(firstSlide).display == "flex";
-// const isSecondSlideActive = getComputedStyle(secondSlide).display == "flex";
-// const isThirdSlideActive = getComputedStyle(thirdSlide).display == "flex";
-// console.log(isFirstSlideActive, isSecondSlideActive, isThirdSlideActive);
-// console.log(isFirstSlideActive);
-    if (getComputedStyle(firstSlide).display == "flex") {
-        firstSlide.style.display = "none";
-        thirdSlide.style.display = "none";
-        secondSlide.style.display = "flex";
-    } else if (getComputedStyle(secondSlide).display == "flex") {
-        secondSlide.style.display = "none";
-        firstSlide.style.display = "none";
-        thirdSlide.style.display = "flex";
-    } else if (getComputedStyle(thirdSlide).display == "flex") {
-        secondSlide.style.display = "none";
-        thirdSlide.style.display = "none";
-        firstSlide.style.display = "flex";
-    }
+  // console.log("Next Button is clicked");
+  // console.log("firstSlide is getting out")
+  // firstSlide.style.display = "none"
+  // console.log("2nd slide is getting in")
+  // secondSlide.style.display = "flex"
+  // const isFirstSlideActive = getComputedStyle(firstSlide).display == "flex";
+  // const isSecondSlideActive = getComputedStyle(secondSlide).display == "flex";
+  // const isThirdSlideActive = getComputedStyle(thirdSlide).display == "flex";
+  // console.log(isFirstSlideActive, isSecondSlideActive, isThirdSlideActive);
+  // console.log(isFirstSlideActive);
+  if (getComputedStyle(firstSlide).display == "flex") {
+    firstSlide.style.display = "none";
+    thirdSlide.style.display = "none";
+    secondSlide.style.display = "flex";
+  } else if (getComputedStyle(secondSlide).display == "flex") {
+    secondSlide.style.display = "none";
+    firstSlide.style.display = "none";
+    thirdSlide.style.display = "flex";
+  } else if (getComputedStyle(thirdSlide).display == "flex") {
+    secondSlide.style.display = "none";
+    thirdSlide.style.display = "none";
+    firstSlide.style.display = "flex";
+  }
 });
-
