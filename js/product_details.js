174076554d911866a2a19b92a084f7a86873f6c5
Mohan <bagan.halder@yahoo.com>
2025-01-30 16:27:57 +0800

    Product Items are selected and bring it in the Chekout Page

--- js/product_details.js
+++ js/product_details.js
@@ -10,12 +10,16 @@ fetch(`https://fakestoreapi.com/products/${value}`)
   .then((product) => {
     console.log(product);
     const productHTML = `
- div class="container px-5 py-24 mx-auto">
+  <div id="product-container" class="container px-5 py-24 mx-auto">
     <div class="lg:w-4/5 mx-auto flex flex-wrap">
       <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="${product.image}">
       <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-        <h2 class="text-sm title-font text-gray-500 tracking-widest">${product.category}</h2>
-        <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">${product.title}</h1>
+        <h2 class="text-sm title-font text-gray-500 tracking-widest">
+        ${product.category}
+        </h2>
+        <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">
+        ${product.title}
+        </h1>
         <div class="flex mb-4">
           <span class="flex items-center">
             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
@@ -59,8 +63,7 @@ fetch(`https://fakestoreapi.com/products/${value}`)
         <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
           <div class="flex">
             <span class="mr-3">Color</span>
-            <button class="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none">
-            </button>
+            <button class="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
             <button class="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
             <button class="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
           </div>
@@ -83,7 +86,7 @@ fetch(`https://fakestoreapi.com/products/${value}`)
         </div>
         <div class="flex">
           <span class="title-font font-medium text-2xl text-gray-900">
-          $ ${product.price}
+          $${product.price}
           </span>
           <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
           <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
